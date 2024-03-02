68ccf5ef94b2e425fbe3d28c03dd31d8685025c3
Ceaseless04 <kristian120304@gmail.com>
2024-03-01 22:07:59 -0500

    Started migrating page.tsx for Jobs route
    - Migrating job.tsx in page Directory from old repo to page.tsx in Jobs directory in new repo
    - Updated @types/react dependency and added NextAuth dependency

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -9,3 +9,35 @@ import { getWasApplyFilterClicked } from "./FilterJobsCard";
 import FilterJobsCard from "./FilterJobsCard";
 import type { FilterInput } from "./FilterJobsCard";
 import JobCard from "./JobCard";
+import { getSession } from 'next-auth/react';
+import superjson from 'superjson';
+import { db } from '~/server/db';
+
+// Define a type fore the selected filters
+export type SelectedFilters = {
+	jobType: string[];
+	jobPosition: string[];
+	jobLocation: string[];
+};
+
+let companyDataArr: CompanyCardType[] | undefined = [];
+
+// GetServerSideProps implementation
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	// Get the authentication context
+	const session = await getSession(context.req);
+
+	// Creeate the context for server-side helpers
+	const trpcContext = {
+		auth, // Auth context
+		db, // Database connection
+	};
+
+	const helpers createServerSideHelpers({
+		router: appRouter,
+		ctx: trpcContext,
+		transformer: superjson,
+	});
+
+	await helpers.jobs.getAll.prefetch();
+};
