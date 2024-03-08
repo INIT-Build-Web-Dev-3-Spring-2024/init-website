4d02945265ffd9709151c8525c63ea6bd967e4b0
Miguel Garcia <112121391+MiguelGarcia2002@users.noreply.github.com>
2024-03-08 17:41:10 -0500

    Working API for jobs page, only have cards working at the moment

--- src/app/api/Events_Tracker/route.ts
+++ src/app/api/Events_Tracker/route.ts
@@ -1,10 +1,10 @@
-'use server'
+import { NextRequest } from "next/server";
+import { jobPostings } from "./jobPostings";
 
-
-export async function POST() {
-	return Response.json({});	
+export async function POST(request: NextRequest) {
+  return new Response(JSON.stringify(""), { status: 200 });
 }
 
-export async function GET() {
-	return Response.json({});
+export async function GET(request: NextRequest) {
+  return new Response(JSON.stringify(jobPostings), { status: 200 });
 }
--- src/app/jobs/JobCard.tsx
+++ src/app/jobs/JobCard.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import React, { useState } from "react";
-import { Card } from "flowbite-react";
+// import { Card } from "flowbite-react";
 import DeleteModal from "./DeleteModal";
 import Image from "next/image";
-import { useUser } from "@clerk/nextjs";
+// import { useUser } from "@clerk/nextjs";
 
 export type Job = {
   id: number;
@@ -16,39 +18,50 @@ export type Job = {
   url: string;
 };
 
-type JobCardProps = {
-  jobPostings: Job[];
-  setJobPostings: React.Dispatch<
-    React.SetStateAction<
-      {
-        date: string;
-        id: number;
-        image: string;
-        title: string;
-        company: string;
-        jobType: string;
-        jobPosition: string;
-        jobLocation: string;
-        url: string;
-      }[]
-    >
-  >;
-};
+// type JobCardProps = {
+//   jobPostings: Job[];
+//   setJobPostings: React.Dispatch<
+//     React.SetStateAction<
+//       {
+//         date: string;
+//         id: number;
+//         image: string;
+//         title: string;
+//         company: string;
+//         jobType: string;
+//         jobPosition: string;
+//         jobLocation: string;
+//         url: string;
+//       }[]
+//     >
+//   >;
+// };
 
-export default function JobCard({ jobPostings, setJobPostings }: JobCardProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedJob, setSelectedJob] = useState<Job | null>(null);
-  const { isSignedIn } = useUser();
+type JobCardProps = { job: Job };
 
-  const openModal = (job: Job) => {
-    setIsModalOpen(true);
-    setSelectedJob(job);
-  };
+export default function JobCard({
+  title,
+  jobType,
+  jobLocation,
+  jobPosition,
+  company,
+  date,
+  image,
+  url,
+}: Job) {
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [selectedJob, setSelectedJob] = useState<Job | null>(null);
+  // // const { isSignedIn } = useUser();
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedJob(null);
-  };
+  // const openModal = (job: Job) => {
+  //   setIsModalOpen(true);
+  //   setSelectedJob(job);
+  // };
+
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  //   setSelectedJob(null);
+  // };
 
   // if (!Array.isArray(jobPostings) || jobPostings.length === 0) {
   //   return <p>No matching   job postingssss.</p>;
@@ -58,21 +71,20 @@ export default function JobCard({ jobPostings, setJobPostings }: JobCardProps) {
     <div
       className={`grid w-full grid-cols-1 place-items-center min-[980px]:grid-cols-2 min-[1320px]:grid-cols-3 `}
     >
-      {jobPostings.map((job, index) => (
-        <Card
-          key={index}
-          className={`min-w-[16rem] max-w-xs bg-[#121415] ${
-            !isSignedIn && "pt-3"
-          } mb-8 border-[#121415] max-[820px]:mb-8`}
-        >
-          <DeleteModal
+      {/* {jobPostings.map((job, index) => ( */}
+      <div
+        className={`min-w-[16rem] max-w-xs bg-[#121415] ${
+          "" // !isSignedIn && "pt-3"
+        } mb-8 border-[#121415] max-[820px]:mb-8`}
+      >
+        {/* <DeleteModal
             cardType="job"
             isOpen={isModalOpen && selectedJob === job}
             onClose={closeModal}
             id={job.id}
             setPostings={setJobPostings}
-          />
-          {isSignedIn && (
+          /> */}
+        {/* {isSignedIn && (
             <div className="flex justify-end text-white">
               <button
                 onClick={() => openModal(job)}
@@ -94,56 +106,56 @@ export default function JobCard({ jobPostings, setJobPostings }: JobCardProps) {
                 </svg>
               </button>
             </div>
-          )}
+          )} */}
 
-          {/* Top of Card */}
-          <div className="mb-1 mt-[-0.5em] flex items-center">
-            <div className="">
-              <Image
-                className="mr-4 h-20 w-20 max-[428px]:h-16 max-[428px]:w-16"
-                src={job.image}
-                alt={job.title}
-                width={1000}
-                height={1000}
-              />
-            </div>
-            <div>
-              <h3 className="mb-1 text-lg text-white max-[428px]:text-[0.84rem]">
-                {job.title}
-              </h3>
-              <p className="text-sm max-[428px]:text-[0.7rem]">{job.company}</p>
-            </div>
+        {/* Top of Card */}
+        <div className="mb-1 mt-[-0.5em] flex items-center">
+          <div className="">
+            <Image
+              className="mr-4 h-20 w-20 max-[428px]:h-16 max-[428px]:w-16"
+              src={image}
+              alt={title}
+              width={1000}
+              height={1000}
+            />
           </div>
-
-          {/* Job Roles Pills */}
-          <div className="mb-6">
-            <span className="mr-2 rounded bg-blue-100 px-2.5 py-1.5 text-sm font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300 max-[428px]:text-[9.25px]">
-              {job.jobType}
-            </span>
-
-            <span className="mr-2 rounded bg-green-100 px-2.5 py-1.5 text-sm font-medium text-green-800 dark:bg-green-900  dark:text-green-300 max-[428px]:text-[9.25px]">
-              {job.jobPosition}
-            </span>
-
-            <span className="mr-2 rounded bg-yellow-100 px-2.5 py-1.5 text-sm font-medium text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300 max-[428px]:text-[9.25px]">
-              {job.jobLocation}
-            </span>
+          <div>
+            <h3 className="mb-1 text-lg text-white max-[428px]:text-[0.84rem]">
+              {title}
+            </h3>
+            <p className="text-sm max-[428px]:text-[0.7rem]">{company}</p>
           </div>
+        </div>
 
-          {/* Date posted and Apply Button */}
-          <div className="mb-4 flex items-center">
-            <h3 className="mr-8 max-[428px]:text-[0.7rem]">{job.date}</h3>
-            <a
-              href={job.url}
-              rel="noopener noreferrer"
-              target="_blank"
-              className="block rounded bg-primary_yellow px-5 py-2 text-sm font-bold text-primary hover:bg-light_yellow focus:ring-light_yellow max-[428px]:px-4 max-[428px]:py-1 max-[428px]:text-[0.8rem]"
-            >
-              Apply
-            </a>
-          </div>
-        </Card>
-      ))}
+        {/* Job Roles Pills */}
+        <div className="mb-6">
+          <span className="mr-2 rounded bg-blue-100 px-2.5 py-1.5 text-sm font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300 max-[428px]:text-[9.25px]">
+            {jobType}
+          </span>
+
+          <span className="mr-2 rounded bg-green-100 px-2.5 py-1.5 text-sm font-medium text-green-800 dark:bg-green-900  dark:text-green-300 max-[428px]:text-[9.25px]">
+            {jobPosition}
+          </span>
+
+          <span className="mr-2 rounded bg-yellow-100 px-2.5 py-1.5 text-sm font-medium text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300 max-[428px]:text-[9.25px]">
+            {jobLocation}
+          </span>
+        </div>
+
+        {/* Date posted and Apply Button */}
+        <div className="mb-4 flex items-center">
+          <h3 className="mr-8 max-[428px]:text-[0.7rem]">{date}</h3>
+          <a
+            href={url}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="block rounded bg-primary_yellow px-5 py-2 text-sm font-bold text-primary hover:bg-light_yellow focus:ring-light_yellow max-[428px]:px-4 max-[428px]:py-1 max-[428px]:text-[0.8rem]"
+          >
+            Apply
+          </a>
+        </div>
+      </div>
+      {/* // ))} */}
     </div>
   );
 }
