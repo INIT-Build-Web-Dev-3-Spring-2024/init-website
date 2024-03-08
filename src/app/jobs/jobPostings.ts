331c506a32d95d4bcc3e0190d078dae0ff9b6ab4
Miguel Garcia <112121391+MiguelGarcia2002@users.noreply.github.com>
2024-03-08 00:47:36 -0500

    added some files from original project of the api folder

---
+++ src/app/jobs/jobPostings.ts
@@ -0,0 +1,74 @@
+export const jobPostings = [
+    {
+      image:
+        "https://res.cloudinary.com/dh6y8bufo/image/upload/v1700202460/init-dashboard/jobs/netflix-logo.png",
+      title: "Software Engineer",
+      company: "Netflix",
+      jobType: "Fulltime",
+      jobPosition: "New-Grad",
+      jobLocation: "On-Site",
+      date: "10/14/23",
+      url: "https://jobs.netflix.com/jobs/291980521",
+    },
+  
+    {
+      image:
+        "https://res.cloudinary.com/dh6y8bufo/image/upload/v1700202362/init-dashboard/jobs/google-logo.png",
+      title: "Software Engineer",
+      company: "Google",
+      jobType: "Fulltime",
+      jobPosition: "Internship",
+      jobLocation: "On-Site",
+      date: "10/11/23",
+      url: "https://www.google.com/about/careers/applications/jobs/results/?src=Online/Google%20Website/ByF&utm_source=Online%20&utm_medium=careers_site%20&utm_campaign=ByF&company=Fitbit&company=Google&company=YouTube&distance=50&employment_type=INTERN",
+    },
+  
+    {
+      image:
+        "https://res.cloudinary.com/dh6y8bufo/image/upload/v1700202587/init-dashboard/jobs/capital-one-logo.svg",
+      title: "Software Engineer",
+      company: "Capital One",
+      jobType: "Fulltime",
+      jobPosition: "Internship",
+      jobLocation: "Remote",
+      date: "10/14/23",
+      url: "https://www.capitalonecareers.com/job/mclean/technology-early-internship-program-summer-2024/31238/52660888800?messenger=email",
+    },
+  
+    {
+      image:
+        "https://res.cloudinary.com/dh6y8bufo/image/upload/v1700202798/init-dashboard/jobs/104477_amazon_icon_b5cspe.png",
+      title: "Software Engineer",
+      company: "Amazon",
+      jobType: "Fulltime",
+      jobPosition: "Internship",
+      jobLocation: "On-Site",
+      date: "10/14/23",
+      url: "https://www.amazon.jobs/en/jobs/2408098/software-development-engineer-internship-2024-us",
+    },
+  
+    {
+      image:
+        "https://res.cloudinary.com/dh6y8bufo/image/upload/v1700202362/init-dashboard/jobs/google-logo.png",
+      title: "Data Science",
+      company: "Google",
+      jobType: "Fulltime",
+      jobPosition: "Internship",
+      jobLocation: "On-Site",
+      date: "10/11/23",
+      url: "https://www.google.com/about/careers/applications/jobs/results/?src=Online/Google%20Website/ByF&utm_source=Online%20&utm_medium=careers_site%20&utm_campaign=ByF&company=Fitbit&company=Google&company=YouTube&distance=50&employment_type=INTERN",
+    },
+  
+    {
+      image:
+        "https://res.cloudinary.com/dh6y8bufo/image/upload/v1700202460/init-dashboard/jobs/netflix-logo.png",
+      title: "Machine Learning",
+      company: "Netflix",
+      jobType: "Fulltime",
+      jobPosition: "New-Grad",
+      jobLocation: "On-Site",
+      date: "10/14/23",
+      url: "https://jobs.netflix.com/jobs/291980521",
+    },
+  ];
+  
