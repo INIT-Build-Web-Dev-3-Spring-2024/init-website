b3aa60d66b5b49dca0d2e01f37ac5cf7527f0ac5
rafa1510 <rcestti78@gmail.com>
2024-03-12 17:15:45 -0400

    refactor: Fix typescript errors

--- src/components/RsvpButton.tsx
+++ src/components/RsvpButton.tsx
@@ -1,16 +1,15 @@
 "use client";
 import React from "react";
 
-export default function RsvpButton({ rsvpLink }) {
-  return (
-    <>
-      <a
-        className="inline-flex w-full items-center justify-center gap-2 rounded-md bg-primary_yellow px-3 py-2 text-xs font-medium text-gray-700 shadow-sm transition-all hover:bg-light_yellow focus:outline-none focus:ring-2 focus:ring-light_yellow sm:rounded-lg sm:px-4 sm:py-3 sm:text-sm"
-        href={rsvpLink}
-        target="_blank"
-      >
-        RSVP Now
-      </a>
-    </>
-  );
+export default function RsvpButton({ rsvpLink }: { rsvpLink: string }) {
+    return (
+        <>
+            <a
+                className="inline-flex w-full items-center justify-center gap-2 rounded-md bg-primary_yellow px-3 py-2 text-xs font-medium text-gray-700 shadow-sm transition-all hover:bg-light_yellow focus:outline-none focus:ring-2 focus:ring-light_yellow sm:rounded-lg sm:px-4 sm:py-3 sm:text-sm"
+                href={rsvpLink}
+                target="_blank">
+                RSVP Now
+            </a>
+        </>
+    );
 }
