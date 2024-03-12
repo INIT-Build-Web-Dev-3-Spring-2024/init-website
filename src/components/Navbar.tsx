03d85e0e0708d580cbf014c2357ea6110346cba9
Smyvens Estime <smyvensestime@gmail.com>
2024-03-12 18:34:50 -0400

    switch to import svgs, fix navlink disapear at certain sizes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa6";
 import { IoMenu } from "react-icons/io5";
+import InitLogo from "@/images/logo.svg";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -57,28 +57,21 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="grid p-8 max-w-screen-2xl mx-auto md:grid-cols-2 lg:grid-cols-3 ">
+    <nav className="grid p-8 max-w-screen-2xl mx-auto lg:grid-cols-3 ">
       <div className="relative mb-7">
         <Link href="/">
-          <Image
-            className="w-20"
-            src="/assets/images/logo.svg"
-            alt="Logo"
-            width={300}
-            height={120}
-            sizes="100vw"
-          />
+          <InitLogo className="h-8 hover:text-primary-yellow" />
         </Link>
 
         <button
-          className="absolute top-0 right-0 md:hidden"
+          className="absolute top-0 right-0 lg:hidden"
           onClick={() => setIsOpen(!isOpen)}
         >
           <IoMenu className="text-3xl" />
         </button>
       </div>
 
-      <div className={twMerge(!isOpen && "hidden", "mb-7 md:hidden lg:block")}>
+      <div className={twMerge(!isOpen && "hidden", "mb-7 lg:block")}>
         <ul className="flex flex-col lg:flex-row items-start lg:items-center justify-between gap-2 lg:gap-0">
           {navLinks.map(({ label, href }) => (
             <li key={label}>
@@ -88,8 +81,8 @@ export default function Navbar() {
         </ul>
       </div>
 
-      <div className={twMerge(!isOpen && "hidden", "md:block")}>
-        <ul className="flex items-center justify-center md:justify-end gap-5">
+      <div className={twMerge(!isOpen && "hidden", "lg:block")}>
+        <ul className="flex items-center justify-start lg:justify-end gap-5">
           {socials.map(({ name, href, Icon, style }) => (
             <li key={name}>
               <Link
