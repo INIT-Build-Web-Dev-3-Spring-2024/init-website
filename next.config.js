4ffbc0085dbdee1fb768bf49e2d567d0f943da7b
rafa1510 <rcestti78@gmail.com>
2024-03-19 20:06:09 -0400

    chore: Change 'domains' config to 'remotePatterns'
    This lets us use the wildcard attribute to allow any AWS link for images
    , and 'remotePatterns' is recommended over 'domains'
    https://nextjs.org/docs/pages/api-reference/components/image#domains

--- next.config.js
+++ next.config.js
@@ -1,19 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ["res.cloudinary.com", "www.freepnglogos.com", "upload.wikimedia.org"],
-    },
-  
-    /**
-     * If you are using `appDir` then you must comment the below `i18n` config out.
-     *
-     * @see https://github.com/vercel/next.js/issues/41980
-     */
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
-    },
-  }
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "www.freepnglogos.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+        port: "",
+      },
+    ],
+  },
 
-module.exports = nextConfig
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
+module.exports = nextConfig;
