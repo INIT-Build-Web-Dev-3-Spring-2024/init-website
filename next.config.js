02f0324e6f93bcba0b51405447aab4daa1359d37
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-03-22 19:57:06 -0400

    Merged main into about-branch

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-        },
-      ],
-    },
-  }
-
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
