0ab246306ca788296e9fbaf20231f2101677cf15
pm-fer <fernipm@gmail.com>
2024-03-22 00:14:33 -0400

    refactor: Changed switch case of program colors and turned it into a constant for readability

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -14,32 +14,22 @@ export interface Event {
   rsvpLink: string;
 }
 
-const programColor = (program: string) => {
-  switch (program) {
-    case "build":
-      return "text-program-build";
-    case "discover":
-      return "text-program-discover";
-    case "explore":
-      return "text-program-explore";
-    case "ignite":
-      return "text-program-ignite";
-    case "hack":
-      return "text-program-hack";
-    case "launch":
-      return "text-program-launch";
-    case "reach":
-      return "text-program-reach";
-    default:
-      return "text-primary-yellow";
-  }
+const color = {
+  build: "text-program-build",
+  discover: "text-program-discover",
+  explore: "text-program-explore",
+  ingite: "text-program-ignite",
+  hack: "text-program-hack",
+  launch: "text-program-launch",
+  reach: "text-program-reach",
+  default: "text-primary-yellow",
 };
 
 export default function EventCard({ picture, description, location, name, program, rsvpLink, date, time }: Event) {
   return (
     <>
-      <div className="group flex h-full flex-col rounded-sm border border-zinc-600 bg-zinc-900 shadow-lg shadow-zinc-900">
-        <div className="relative h-52 overflow-hidden rounded-t-sm sm:h-56">
+      <div className="group flex h-full flex-col rounded-2xl bg-zinc-900 overflow-hidden">
+        <div className="relative w-full h-52">
           {
             <Image
               src={picture}
@@ -50,16 +40,16 @@ export default function EventCard({ picture, description, location, name, progra
             />
           }
         </div>
-        <div className="flex flex-grow flex-col justify-between p-4 md:p-6">
+        <div className="flex flex-grow flex-col font-semibold justify-between p-4 md:p-6">
           <div>
-            <span className={`mb-1 block text-xs font-semibold uppercase ${programColor(program.toLowerCase())}`}>
+            <span className={`mb-1 block text-xs font-semibold uppercase ${color[program.toLowerCase() as keyof typeof color] || color.default}`}>
               {program}
             </span>
             <h3 className="text-lg font-semibold text-gray-300 sm:text-xl">{name}</h3>
             <p className="sm:text-md mt-2 text-sm text-gray-500">{description}</p>
-            <div>
-              <p className="text-gray-400">{date}</p>
-              <p className="text-xs text-gray-400">
+            <div className="mt-2 space-y-1 text-sm text-gray-400 sm:mt-4">
+              <p>{date}</p>
+              <p className="text-xs">
                 {location} | {time}
               </p>
             </div>
