2197f7a36a625fe05ee94ad6e4c236e23a06fe30
pm-fer <fernipm@gmail.com>
2024-03-22 00:18:32 -0400

    chore: Update Events page styling so it follows the old version

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -5,15 +5,13 @@ export default async function page() {
   const events = await fetchEvents(true);
 
   return (
-    <>
-      {" "}
-      <div className="mx-auto mt-8">
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-          {events?.map((event) => (
-            <EventCard key={event.id} {...event}></EventCard>
-          ))}
-        </div>
+    <main className="bg-zinc-900 min-h-screen px-16 py-12 bg-opacity-95">
+      <h1 className="text-4xl font-bold mb-3 text-white">Upcoming events</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 mx-auto mt-8">
+        {events?.map((event) => (
+          <EventCard key={event.id} {...event}></EventCard>
+        ))}
       </div>
-    </>
+    </main>
   );
 }
