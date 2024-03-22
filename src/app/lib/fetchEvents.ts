a29a6e361dbad843040e835c679337bfd8620d90
rafa1510 <rcestti78@gmail.com>
2024-03-22 15:56:28 -0400

    chore: fix default image link & clean up comments

--- src/app/lib/fetchEvents.ts
+++ src/app/lib/fetchEvents.ts
@@ -106,10 +106,11 @@ export default async function fetchEvents(searchQuery = "", weekly = false) {
             let location = "Location TBD";
             let description = "";
             let rsvpLink = "RSVP TBD";
-            let picture = "/eventDefaultImage.avif"; // Default Event Image, can be a link or a local file
+            let picture = "/assets/images/eventDefaultImage.avif"; // Default Event Image, can be a link or a local file
             let program = "General";
 
-            // Check and extract the 'Project name' property
+            // Check and extract the properties from Notion
+
             const titleProperty = page.properties["﻿Project name"]; // `Project name` on Notion database has an invisible character for some reason
             if (
               titleProperty?.type === "title" &&
@@ -119,7 +120,6 @@ export default async function fetchEvents(searchQuery = "", weekly = false) {
               name = titleProperty.title[0].plain_text;
             }
 
-            // Check and extract the 'Date' property
             const dateProperty = page.properties["Date"];
             if (dateProperty?.type === "date" && dateProperty.date) {
               let dateObject = new Date(dateProperty.date.start);
@@ -140,7 +140,6 @@ export default async function fetchEvents(searchQuery = "", weekly = false) {
               }
             }
 
-            // Check and extract the 'Location' property
             const locationProperty = page.properties["Location"];
             if (
               locationProperty?.type === "rich_text" &&
@@ -150,7 +149,6 @@ export default async function fetchEvents(searchQuery = "", weekly = false) {
               location = locationProperty.rich_text[0].plain_text;
             }
 
-            // Check and extract the 'Description' property
             const descriptionProperty = page.properties["Description"];
             if (
               descriptionProperty?.type === "rich_text" &&
@@ -160,13 +158,11 @@ export default async function fetchEvents(searchQuery = "", weekly = false) {
               description = descriptionProperty.rich_text[0].plain_text;
             }
 
-            // Check and extract the 'Luma' property
             const lumaProperty = page.properties["Luma"];
             if (lumaProperty?.type === "url" && typeof lumaProperty.url === "string") {
               rsvpLink = lumaProperty.url;
             }
 
-            // Check and extract the 'Picture' property
             const pictureProperty = page.properties["Picture"];
             if (
               pictureProperty?.type === "files" &&
@@ -177,7 +173,6 @@ export default async function fetchEvents(searchQuery = "", weekly = false) {
               picture = pictureProperty.files[0].file.url;
             }
 
-            // Check and extract the "Role" property, will be used to get the program
             const programProperty = page.properties["Role"];
             if (
               programProperty?.type === "rich_text" &&
@@ -185,7 +180,7 @@ export default async function fetchEvents(searchQuery = "", weekly = false) {
               programProperty.rich_text.length > 0 &&
               "text" in programProperty.rich_text[0]
             ) {
-              // Extract property from notion, and match against our regex
+              // Match role property from Notion against our Regex to get program name
               const programMatch: RegExpMatchArray | null = programProperty.rich_text[0].text.content.match(regex);
 
               // If a program is found (Reach, Build, etc.), then store the name in `program`
