c937bde835c9ed66fa9de5ecad62f4feeb18cd3c
Dimitri Pierre-Louis <dimitripl6@gmail.com>
2024-03-22 18:15:14 -0400

    Resolving merge conflicts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: { and: [/\.(js|ts)x?$/] },
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,33 +5,30 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#1A1E22",
-        primary_yellow: "#FFD550",
-        discord_purple: "#7289DA",
-        light_yellow: "#FFF1B8",
-        primary_gray: "#D1D1D1",
-        secondary: "#FFFFFF",
-        light_purple: "#D0C3FC",
-        build: "#FC6C3F",
-        discover: "#FF3E9E",
-        explore: "#EB6A65",
-        ignite: "#87DE65",
-        hack: "#A27BFC",
-        launch: "#11CEBB",
-        reach: "#6FA4F4",
-      },
-      height: {
-        "128": "32rem", // Adds h-128 for 32rem/512px height
-        "144": "36rem", // Adds h-144 for 36rem/576px height
-        "160": "40rem", // Adds h-160 for 40rem/640px height
-      },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        primary: {
+          DEFAULT: "#1A1E22",
+          yellow: "#FFD550",
+          purple: "#7289DA",
+          gray: "#D1D1D1",
+        },
+        secondary: {
+          DEFAULT: "#FFFFFF",
+          yellow: "#FFF1B8",
+          purple: "#D0C3FC",
+        },
+        program: {
+          build: "#FC6C3F",
+          discover: "#FF3E9E",
+          explore: "#EB6A65",
+          ignite: "#87DE65",
+          hack: "#A27BFC",
+          launch: "#11CEBB",
+          reach: "#6FA4F4",
+        },
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -40,8 +37,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-
-],
+  plugins: [],
 };
 export default config;
