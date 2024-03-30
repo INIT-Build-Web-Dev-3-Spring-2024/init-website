1c0e0c146ab2abedce2d4098d0168ab9203977dd
Carol <apowe056@fiu.edu>
2024-03-29 20:25:51 -0400

    "Get INIT" and "Discord" buttons were added, computer image with centered vertical line were also added

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+import { Title, AnimatedTitle } from "@/components/Title";
 import ProgramCard from "./programCard";
+import Image from "next/image";
+import Text from "@/components/Text";
+import Button from "@/components/Button";
 
-export default async function ProgramsPage() {
+export default 
+// async
+ function ProgramsPage() {
   interface Program {
     name: string;
     color: string;
@@ -8,23 +15,48 @@ export default async function ProgramsPage() {
     description: string;
   }
 
-  const programRequest = await fetch("http://localhost:3000/api/programs");
-  const programs: Program[] = await programRequest.json();
+  // const programRequest = await fetch("http://localhost:3000/api/programs");
+  // const programs: Program[] = await programRequest.json();
 
   return (
-    <div className="bg-zinc-900 min-h-screen px-16 py-12 bg-opacity-95">
-      <h1 className="text-4xl font-bold mb-3 text-white">Programs</h1>
-      <div className="grid grid-cols-3 gap-6 lg:grid-cols-4">
-        {programs.map(({ name, color, image, description }) => (
-          <ProgramCard
-            key={name}
-            name={name}
-            color={color}
-            image={image}
-            description={description}
-          />
-        ))}
+    <>
+      <div className="flex flex-col justify-center mt-8 w-[300px] ml-8">
+        <div className="mx-auto w-1 h-[100px] bg-white" />
+
+        <Image
+          className="mb-8"
+          src="images/testimonial.svg"
+          alt="Picture of computers"
+          width={300}
+          height={300}
+        />
       </div>
-    </div>
+
+      <Title>
+        Become a <AnimatedTitle>Member</AnimatedTitle>
+      </Title>
+
+      <Text className="w-[80vw] m-auto mt-8">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, laborum
+        numquam aspernatur aliquid fugiat quo fugit sit cum temporibus esse
+        explicabo quia. Repudiandae aliquid, voluptas repellendus ipsa modi sint
+        error.
+      </Text>
+
+      <div className="flex justify-center mt-8">
+        <Button gradientBorder onClick={() => null} className="">
+          Get INIT
+        </Button>
+        <Button href="" className="flex justify-center gap-2">
+          <Image
+            src="images/discord-icon.svg"
+            alt="Picture of discord"
+            width={15}
+            height={15}
+          />
+          Discord
+        </Button>
+      </div>
+    </>
   );
 }
