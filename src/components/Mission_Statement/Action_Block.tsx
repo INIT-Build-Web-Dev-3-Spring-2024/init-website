eef86d2cfdad4a308a9a37c443e5a1fce659d8a2
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-04-02 19:28:07 -0400

    Responsive Update
    -Added responsiveness to majority of component
    -Filled in the paragraphs with corresponding data

--- src/components/Mission_Statement/Action_Block.tsx
+++ src/components/Mission_Statement/Action_Block.tsx
@@ -1,8 +1,6 @@
-import Develop from "@/images/develop_icon.svg";
 import SubTitle from "../SubTitle";
 import Text from "../Text";
 import Astronaut from "@/images/Astronaut.svg";
-import Circle from "@/images/large_circle_icon.svg";
 import Phone from "@/images/phone-icon.svg";
 import Rocket from "@/images/rocket_icon.svg";
 import People from "@/images/people_icon.svg";
@@ -13,41 +11,45 @@ export default function Action_Block() {
     {
       name: "Develop",
       description:
-        "An in-depth guide to understanding the principles, structures, and benefits.",
+        "Work on projects with fellow students and professionals leveraging current technologies and practices used in the industry.",
       Icon: Phone,
     },
     {
-      name: "Enjoy",
+      name: "Network",
       description:
-        "An in-depth guide to understanding the principles, structures, and benefits.",
+        "Our partnerships with employers connect you with recruiters and help you land your first role in tech.",
       Icon: Rocket,
     },
     {
       name: "Meet",
       description:
-        "An in-depth guide to understanding the principles, structures, and benefits.",
+        "Our inclusive community of students and professionals helps you navigate the tech industry",
       Icon: People,
     },
     {
       name: "Learn",
       description:
-        "An in-depth guide to understanding the principles, structures, and benefits.",
+        "Our experiential learning and career development programs prepare you for new opportunities.",
       Icon: Gradcap,
     },
   ];
 
   return (
     <>
-      <div className="grid grid-cols-2 gap-y-10 my-10 w-screen max-md:grid-cols-1">
+      <div className="grid grid-cols-2 gap-y-10 my-10 w-screen max-xl:grid-cols-1">
         <div className="grid grid-cols-1 gap-y-10 max-md:w-full">
           <Astronaut className="grid max-md:w-full" />
         </div>
-        <div className="grid grid-cols-2 gap-y-10 my-60 ml-40 w-4/6 max-md:grid-cols-1">
+        <div className="grid grid-cols-2 gap-y-10 my-60 ml-20 w-5/6 max-xl:grid-cols-1 max-xl:mx-auto max-xl:w-1/2">
           {items.map(({ name, description, Icon }) => (
             <div key={name}>
-              <Icon className="my-2 w-2/5" />
-              <SubTitle className="my-4 w-1/5">{name}</SubTitle>
-              <Text className="my-4 w-1/2">{description}</Text>
+              <Icon className="my-2 w-2/5 max-xl:mx-auto max-xl:w-1/5 max-md:w-2/6 max-md:mx-36" />
+              <SubTitle className="my-4 w-2/5 max-xl:mx-auto max-xl:w-1/5">
+                {name}
+              </SubTitle>
+              <Text className="my-4 w-5/6 max-xl:mx-auto max-xl:w-full max-md:mx-5">
+                {description}
+              </Text>
             </div>
           ))}
         </div>
