e6b62e6dc086ad894bc16fa2bb698a2f2caab5bc
rafa1510 <rcestti78@gmail.com>
2024-04-04 14:18:14 -0400

    feature: add in two images per program, and skillsDescription

--- src/app/programs/AboutProgram.tsx
+++ src/app/programs/AboutProgram.tsx
@@ -1,58 +1,75 @@
-import InitLogo from "@/components/InitLogo";
-import Text from "@/components/Text";
-import { Title } from "@/components/Title";
-import Image from "next/image";
-import { twMerge } from "tailwind-merge";
-import tailwindConfig from "@/root/tailwind.config";
-import resolveConfig from "tailwindcss/resolveConfig";
+import InitLogo from '@/components/InitLogo'
+import Text from '@/components/Text'
+import { Title } from '@/components/Title'
+import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
+import tailwindConfig from '@/root/tailwind.config'
+import resolveConfig from 'tailwindcss/resolveConfig'
 
-const twConfig = resolveConfig(tailwindConfig);
+const twConfig = resolveConfig(tailwindConfig)
 
 export interface AboutProgramProps {
-  name: string;
-  description: string;
-  images: string[];
+  name: string
+  description: string
+  skillsDescription: string
+  images: string[]
 }
 
 export default function AboutProgram(props: AboutProgramProps) {
-  const { name, description, images } = props;
+  const { name, description, skillsDescription, images } = props
 
   function getColor(program: string) {
-    const programName = program as keyof typeof twConfig.theme.colors.program; // so typescript can stop
-    return twConfig.theme.colors.program[programName];
+    const programName = program as keyof typeof twConfig.theme.colors.program // so typescript can stop
+    return twConfig.theme.colors.program[programName]
   }
 
   return (
     <>
-      <div className="flex flex-col justify-center mt-8 w-[300px] ml-8">
-        <div
-          className={twMerge("mx-auto w-[1px] h-[100px] mb-8")}
-          style={{
-            backgroundColor: getColor(name.toLowerCase()),
-          }}
-        />
-        <Title className="flex items-center gap-2">
-          <span>
-            <InitLogo className="w-24 h-24 mb-[10px]" />
-          </span>
-          <span style={{ color: getColor(name.toLowerCase()) }}>{name}</span>
-        </Title>
-        <Text className="mt-4 text-sm mb-8">{description}</Text>
-        <div
-          className="mx-auto w-[1px] h-[100px]"
-          style={{
-            backgroundColor: getColor(name.toLowerCase()),
-          }}
-        />
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col justify-center mt-8 w-[300px] ml-8">
+          <div
+            className={twMerge('mx-auto w-[1px] h-[300px] mb-8')}
+            style={{
+              backgroundColor: getColor(name.toLowerCase()),
+            }}
+          />
+          <Title className="flex items-center gap-2">
+            <span>
+              <InitLogo className="w-24 h-24 mb-[10px]" />
+            </span>
+            <span style={{ color: getColor(name.toLowerCase()) }}>{name}</span>
+          </Title>
+          <Text className="mt-4 text-sm mb-8">{description}</Text>
+          <div
+            className="mx-auto w-[1px] h-[300px] mb-8"
+            style={{
+              backgroundColor: getColor(name.toLowerCase()),
+            }}
+          />
 
-        <Image
-          className="mb-8"
-          src="images/testimonial.svg"
-          alt="Picture of computers"
-          width={300}
-          height={300}
-        />
+          <Image className="mb-8" src="images/testimonial.svg" alt="Picture of computers" width={300} height={300} />
+        </div>
+        <div className="flex flex-col justify-center mt-8 w-[500px] ml-8">
+          <Image
+            className="mb-8 h-48 object-cover rounded-lg translate-x-24 z-10"
+            src={images[0]}
+            alt="Picture of computers"
+            width={300}
+            height={192}
+          />
+          <Image
+            className="mb-8 h-48 object-cover rounded-lg -translate-y-16 -translate-x-24"
+            src={images[1]}
+            alt="Picture of computers"
+            width={300}
+            height={192}
+          />
+          <Title className="flex items-center gap-2">
+            <Text>Learn Skills</Text>
+          </Title>
+          <Text>{skillsDescription}</Text>
+        </div>
       </div>
     </>
-  );
+  )
 }
--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,59 +1,57 @@
-"use client";
+'use client'
 
-import CallToAction from "@/components/CallToAction";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
-import AboutProgram, { AboutProgramProps } from "./AboutProgram";
-import ProgramCirclePicker from "./ProgramCirclePicker";
-import BuildProjectGallery from "./(BuildProjectGallery)";
+import CallToAction from '@/components/CallToAction'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+import AboutProgram, { AboutProgramProps } from './AboutProgram'
+import ProgramCirclePicker from './ProgramCirclePicker'
+import BuildProjectGallery from './(BuildProjectGallery)'
 
-const programs: Record<string, Omit<AboutProgramProps, "name">> = {
+const programs: Record<string, Omit<AboutProgramProps, 'name'>> = {
   Explore: {
-    description:
-      "Explore the field of technology through introductory technical workshops",
-    images: [],
+    description: 'Explore the field of technology through introductory technical workshops',
+    skillsDescription: 'Explain what Explore does and teach',
+    images: ['/images/programCards/explore/exploreOne.webp', '/images/programCards/explore/exploreTwo.webp'],
   },
   Reach: {
-    description:
-      "Reach your career goals by preparing for interviews and fine-tuning your resume",
-    images: [],
+    description: 'Reach your career goals by preparing for interviews and fine-tuning your resume',
+    skillsDescription: 'Explain what Reach does and teach',
+    images: ['/images/programCards/reach/reachOne.webp', '/images/programCards/reach/reachTwo.webp'],
   },
   Build: {
-    description:
-      "Build technical projects by applying your skills and gain experience",
-    images: [],
+    description: 'Build technical projects by applying your skills and gain experience',
+    skillsDescription: 'Explain what Build does and teach',
+    images: ['/images/programCards/build/buildOne.webp', '/images/programCards/build/buildTwo.webp'],
   },
   Hack: {
-    description:
-      "Hack into a career by learning, building, and networking at hackathons",
-    images: [],
+    description: 'Hack into a career by learning, building, and networking at hackathons',
+    skillsDescription: 'Explain what Hack does and teach',
+    images: ['/images/programCards/hack/hackOne.webp', '/images/programCards/hack/hackTwo.webp'],
   },
   Ignite: {
-    description:
-      "Ignite the passion for technology by teaching and inspiring the youth",
-    images: [],
+    description: 'Ignite the passion for technology by teaching and inspiring the youth',
+    skillsDescription: 'Explain what Ignite does and teach',
+    images: ['/images/programCards/ignite/igniteOne.webp', '/images/programCards/ignite/igniteTwo.webp'],
   },
   Uplift: {
-    description:
-      "Uplift others looking to start their journey into tech through peer mentorship",
-    images: [],
+    description: 'Uplift others looking to start their journey into tech through peer mentorship',
+    skillsDescription: 'Explain what Uplift does and teach',
+    images: ['/images/programCards/uplift/upliftOne.webp', '/images/programCards/uplift/upliftTwo.webp'],
   },
-};
+}
 
 export default function ProgramsPage() {
-  const [currentProgram] = useLocalStorage("view");
+  const [currentProgram] = useLocalStorage('view')
 
   function pickView() {
     switch (currentProgram) {
       case null:
-        return;
+        return
 
-      case "Build":
-        return <BuildProjectGallery />;
+      case 'Build':
+        return <BuildProjectGallery />
 
       default:
-        return (
-          <AboutProgram {...programs[currentProgram]} name={currentProgram} />
-        );
+        return <AboutProgram {...programs[currentProgram]} name={currentProgram} />
     }
   }
 
@@ -65,5 +63,5 @@ export default function ProgramsPage() {
 
       <CallToAction />
     </>
-  );
+  )
 }
