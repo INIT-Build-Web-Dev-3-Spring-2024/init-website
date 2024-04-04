bd680d9b8c6a4f936124c257423b1c1132c4eb27
rafa1510 <rcestti78@gmail.com>
2024-04-03 22:07:43 -0400

    chore: add in 13 other reviews, and a function to get a random number

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -56,6 +56,10 @@ function Review({ person }: { person: Person }) {
   );
 }
 
+function getRandomNum(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
 export default function Testimonials() {
   // if using links for images, make sure to add hostname to `next.config.js`
   const reviews = [
@@ -128,6 +132,135 @@ export default function Testimonials() {
       position: "Supply Chain Analyst",
       review:
         "GlobalLogistics is transforming the supply chain industry, and I'm thrilled to contribute to such groundbreaking work.",
+    },
+    {
+      index: 7,
+      name: "Person Eight",
+      avatarSrc: "/images/reviews/personEight/personEightAvatar.svg",
+      company: "QuantumLeap",
+      companyLogoSrc: "/images/reviews/personEight/personEightCompany.svg",
+      position: "Quantum Computing Engineer",
+      review:
+        "At QuantumLeap, I'm working with some of the brightest minds to solve complex problems using quantum computing.",
+    },
+    {
+      index: 8,
+      name: "Person Nine",
+      avatarSrc: "/images/reviews/personNine/personNineAvatar.svg",
+      company: "GreenArchitects",
+      companyLogoSrc: "/images/reviews/personNine/personNineCompany.svg",
+      position: "Sustainable Design Architect",
+      review:
+        "GreenArchitects is pioneering in sustainable building designs, and it's inspiring to contribute to eco-friendly architecture.",
+    },
+    {
+      index: 9,
+      name: "Person Ten",
+      avatarSrc: "/images/reviews/personTen/personTenAvatar.svg",
+      company: "DataDive",
+      companyLogoSrc: "/images/reviews/personTen/personTenCompany.svg",
+      position: "Data Science Lead",
+      review:
+        "DataDive is a data enthusiast's paradise, offering endless opportunities to uncover insights and drive innovation.",
+    },
+    {
+      index: 10,
+      name: "Person Eleven",
+      avatarSrc: "/images/reviews/personEleven/personElevenAvatar.svg",
+      company: "NutriGrow",
+      companyLogoSrc: "/images/reviews/personEleven/personElevenCompany.svg",
+      position: "Agricultural Biotechnologist",
+      review:
+        "NutriGrow is revolutionizing agriculture with biotech, and I'm proud to be part of creating sustainable food solutions.",
+    },
+    {
+      index: 11,
+      name: "Person Twelve",
+      avatarSrc: "/images/reviews/personTwelve/personTwelveAvatar.svg",
+      company: "MindMesh",
+      companyLogoSrc: "/images/reviews/personTwelve/personTwelveCompany.svg",
+      position: "AI Psychologist",
+      review:
+        "Working at MindMesh to develop empathetic AI is an exciting blend of technology and psychology, offering fresh challenges daily.",
+    },
+    {
+      index: 12,
+      name: "Person Thirteen",
+      avatarSrc: "/images/reviews/personThirteen/personThirteenAvatar.svg",
+      company: "CleanOcean",
+      companyLogoSrc: "/images/reviews/personThirteen/personThirteenCompany.svg",
+      position: "Marine Conservation Strategist",
+      review:
+        "At CleanOcean, my work on marine conservation projects is deeply fulfilling and contributes to vital environmental protection.",
+    },
+    {
+      index: 13,
+      name: "Person Fourteen",
+      avatarSrc: "/images/reviews/personFourteen/personFourteenAvatar.svg",
+      company: "SkyInnovations",
+      companyLogoSrc: "/images/reviews/personFourteen/personFourteenCompany.svg",
+      position: "Aerospace Design Engineer",
+      review:
+        "SkyInnovations is pushing the boundaries of aerospace technology, and I'm thrilled to be part of such an ambitious team.",
+    },
+    {
+      index: 14,
+      name: "Person Fifteen",
+      avatarSrc: "/images/reviews/personFifteen/personFifteenAvatar.svg",
+      company: "UrbanEco",
+      companyLogoSrc: "/images/reviews/personFifteen/personFifteenCompany.svg",
+      position: "Urban Planner",
+      review:
+        "UrbanEco's commitment to sustainable urban development is unmatched, and I'm learning and contributing to real change.",
+    },
+    {
+      index: 15,
+      name: "Person Sixteen",
+      avatarSrc: "/images/reviews/personSixteen/personSixteenAvatar.svg",
+      company: "GeneEdit",
+      companyLogoSrc: "/images/reviews/personSixteen/personSixteenCompany.svg",
+      position: "Genetic Engineer",
+      review:
+        "GeneEdit is at the cutting edge of genetic engineering, offering unparalleled opportunities to be at the forefront of medical innovation.",
+    },
+    {
+      index: 16,
+      name: "Person Seventeen",
+      avatarSrc: "/images/reviews/personSeventeen/personSeventeenAvatar.svg",
+      company: "FutureFinance",
+      companyLogoSrc: "/images/reviews/personSeventeen/personSeventeenCompany.svg",
+      position: "Cryptocurrency Analyst",
+      review:
+        "At FutureFinance, delving into the world of cryptocurrencies offers an exciting and ever-evolving financial landscape to explore.",
+    },
+    {
+      index: 17,
+      name: "Person Eighteen",
+      avatarSrc: "/images/reviews/personEighteen/personEighteenAvatar.svg",
+      company: "VRVisions",
+      companyLogoSrc: "/images/reviews/personEighteen/personEighteenCompany.svg",
+      position: "Virtual Reality Developer",
+      review: "Creating immersive virtual worlds at VRVisions is as thrilling as it is technically challenging.",
+    },
+    {
+      index: 18,
+      name: "Person Nineteen",
+      avatarSrc: "/images/reviews/personNineteen/personNineteenAvatar.svg",
+      company: "RoboCraft",
+      companyLogoSrc: "/images/reviews/personNineteen/personNineteenCompany.svg",
+      position: "Robotics Engineer",
+      review:
+        "RoboCraft is defining the future of robotics, and being part of this journey is incredibly rewarding and exciting.",
+    },
+    {
+      index: 19,
+      name: "Person Twenty",
+      avatarSrc: "/images/reviews/personTwenty/personTwentyAvatar.svg",
+      company: "NextGenEdu",
+      companyLogoSrc: "/images/reviews/personTwenty/personTwentyCompany.svg",
+      position: "Educational Technologist",
+      review:
+        "At NextGenEdu, I'm part of a team that's reimagining how technology can transform education, making it more accessible and engaging for everyone.",
     },
   ];
 
