30abf148f004ef30a1fd2eafb59d1823d1bb109f
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-04-05 18:41:49 -0400

    moved LiveEvents to home folder

---
+++ src/app/home/(LiveEvents)/LiveIndicator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IoMdRadioButtonOn } from 'react-icons/io';
+
+const LiveIndicator = () => {
+  return (
+    <div className="m-0 flex items-center">
+      <IoMdRadioButtonOn className="text-red-500" size="1em" />
+      <span className="ml-1 text-white">Live</span>
+    </div>
+  );
+};
+
+export default LiveIndicator;
---
+++ src/app/home/LiveEvents/NextEvent.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
+import Text from "../../../components/Text";
+import SubTitle from "../../../components/SubTitle";
+
+interface NextEvent {
+  title: string;
+  description: string;
+  onNext: () => void;
+}
+
+const NextEvent = (props: NextEvent) => {
+  return (
+    <div className="text-white flex justify-between bg-gradient-to-r from-purple-500 to-pink-500 w-1/3 rounded-lg p-3 ml-auto">
+      <div>
+        <SubTitle className="font-bold">{props.title}</SubTitle>
+        <Text className="text-sm">
+          {props.description.slice(0, 100) + "..."}
+        </Text>
+      </div>
+      <button className="rounded-full" onClick={props.onNext}>
+        <IoIosArrowForward className="text-lg" />
+      </button>
+    </div>
+  );
+};
+
+export default NextEvent;
