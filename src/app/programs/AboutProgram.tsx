059e669596a5f8d0fc836f0f1d6e529cc2166f14
rafa1510 <rcestti78@gmail.com>
2024-04-04 22:19:46 -0400

    chore: add some background blobs

--- src/app/programs/AboutProgram.tsx
+++ src/app/programs/AboutProgram.tsx
@@ -41,6 +41,7 @@ export default function AboutProgram(props: AboutProgramProps) {
               <span style={{ color: getColor(name.toLowerCase()) }}>{name}</span>
             </Title>
             <Text className="mt-4 text-sm mb-8">{description}</Text>
+
             <div
               className={twMerge('absolute -bottom-[350px] left-1/4 mx-auto w-px h-[300px] mb-8')}
               style={{
@@ -48,10 +49,11 @@ export default function AboutProgram(props: AboutProgramProps) {
               }}
             />
           </div>
-
+          <Image className="absolute left-0 -translate-y-72 -z-10" src="images/blobTwo.svg" alt="Blob One" width={400} height={400} />
           <Image className="mb-8 mt-[350px]" src="images/testimonial.svg" alt="Picture of computers" width={300} height={300} />
         </div>
         <div className="flex flex-col justify-center">
+          <Image className="absolute right-0 -translate-y-28" src="images/blobOne.svg" alt="Blob One" width={500} height={500} />
           <Image
             className="mb-8 h-56 w-96 object-cover object-left-top rounded-lg self-end z-10 -translate-x-1/3"
             src={images[0]}
