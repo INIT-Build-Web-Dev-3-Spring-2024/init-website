b656e7d7b5212d079845274437a68e68b31c4dd7
rafa1510 <rcestti78@gmail.com>
2024-04-04 21:41:51 -0400

    chore: change program images alt text

--- src/app/programs/AboutProgram.tsx
+++ src/app/programs/AboutProgram.tsx
@@ -1,26 +1,26 @@
-import InitLogo from "@/components/InitLogo";
-import Text from "@/components/Text";
-import { Title } from "@/components/Title";
-import Image from "next/image";
-import { twMerge } from "tailwind-merge";
-import tailwindConfig from "@/root/tailwind.config";
-import resolveConfig from "tailwindcss/resolveConfig";
+import InitLogo from '@/components/InitLogo'
+import Text from '@/components/Text'
+import { Title } from '@/components/Title'
+import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
+import tailwindConfig from '@/root/tailwind.config'
+import resolveConfig from 'tailwindcss/resolveConfig'
 
-const twConfig = resolveConfig(tailwindConfig);
+const twConfig = resolveConfig(tailwindConfig)
 
 export interface AboutProgramProps {
-  name: string;
-  description: string;
-  skillsDescription: string;
-  images: string[];
+  name: string
+  description: string
+  skillsDescription: string
+  images: string[]
 }
 
 export default function AboutProgram(props: AboutProgramProps) {
-  const { name, description, skillsDescription, images } = props;
+  const { name, description, skillsDescription, images } = props
 
   function getColor(program: string) {
-    const programName = program as keyof typeof twConfig.theme.colors.program; // so typescript can stop
-    return twConfig.theme.colors.program[programName];
+    const programName = program as keyof typeof twConfig.theme.colors.program // so typescript can stop
+    return twConfig.theme.colors.program[programName]
   }
 
   return (
@@ -29,9 +29,7 @@ export default function AboutProgram(props: AboutProgramProps) {
         <div className="">
           <div className="relative">
             <div
-              className={twMerge(
-                "absolute -top-[200px] left-1/4 mx-auto w-px h-[200px] mb-8"
-              )}
+              className={twMerge('absolute -top-[200px] left-1/4 mx-auto w-px h-[200px] mb-8')}
               style={{
                 backgroundColor: getColor(name.toLowerCase()),
               }}
@@ -40,41 +38,31 @@ export default function AboutProgram(props: AboutProgramProps) {
               <span>
                 <InitLogo className="w-24 h-24 mb-3" />
               </span>
-              <span style={{ color: getColor(name.toLowerCase()) }}>
-                {name}
-              </span>
+              <span style={{ color: getColor(name.toLowerCase()) }}>{name}</span>
             </Title>
             <Text className="mt-4 text-sm mb-8">{description}</Text>
             <div
-              className={twMerge(
-                "absolute -bottom-[350px] left-1/4 mx-auto w-px h-[300px] mb-8"
-              )}
+              className={twMerge('absolute -bottom-[350px] left-1/4 mx-auto w-px h-[300px] mb-8')}
               style={{
                 backgroundColor: getColor(name.toLowerCase()),
               }}
             />
           </div>
 
-          <Image
-            className="mb-8 mt-[350px]"
-            src="images/testimonial.svg"
-            alt="Picture of computers"
-            width={300}
-            height={300}
-          />
+          <Image className="mb-8 mt-[350px]" src="images/testimonial.svg" alt="Picture of computers" width={300} height={300} />
         </div>
         <div className="flex flex-col justify-center">
           <Image
             className="mb-8 h-56 w-96 object-cover object-left-top rounded-lg self-end z-10 -translate-x-1/3"
             src={images[0]}
-            alt="Picture of computers"
+            alt={`INIT ${name} Photo`}
             width={300}
             height={192}
           />
           <Image
             className="mb-8 h-56 w-96  object-cover object-left-top rounded-lg -translate-y-16 self-end -translate-x-3/4"
             src={images[1]}
-            alt="Picture of computers"
+            alt={`INIT ${name} Photo`}
             width={300}
             height={192}
           />
@@ -85,5 +73,5 @@ export default function AboutProgram(props: AboutProgramProps) {
         </div>
       </div>
     </>
-  );
+  )
 }
